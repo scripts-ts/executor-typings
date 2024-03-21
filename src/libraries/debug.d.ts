7a2d7b93bf21ce51b55d3c23a25ab178ee00ee83
TerminalVibes <87617218+TerminalVibes@users.noreply.github.com>
2024-03-21 16:32:41 -0400

    Last fix, trust

--- src/libraries/debug.d.ts
+++ src/libraries/debug.d.ts
@@ -1,9 +1,9 @@
 interface ExecutorDebug {
-	getconstants(func: (...args: any[]) => any | number): Array<any>;
+	getconstants: (func: (...args: any[]) => any | number) => Array<any>;
 
-	getinfo<T extends (...args: any[]) => any>(
+	getinfo: <T extends (...args: any[]) => any>(
 		func: T | number,
-	): {
+	) => {
 		name: "Lua" | "C";
 		what: string;
 		func: (...args: any[]) => any;
@@ -19,15 +19,15 @@ interface ExecutorDebug {
 	 * Returns the upvalue at index in the function or level func. Throws an error if the upvalue does not exist.
 	 * An upvalue is a local variable used by an inner function, and is also called an external local variable.
 	 */
-	getupvalue(func: (...args: any[]) => any | number, index?: number): any;
+	getupvalue: (func: (...args: any[]) => any | number, index?: number) => any;
 
 	/**
 	 * Returns a list of upvalues of the function or level `func`.
 	 */
-	getupvalues(func: (...args: any[]) => any | number): Array<any>;
+	getupvalues: (func: (...args: any[]) => any | number) => Array<any>;
 
 	/**
 	 * Sets the value of the upvalue with the index `index` of the function `func` to `value`.
 	 */
-	setupvalue(func: (...args: any[]) => any | number, index: number, value: any): void;
+	setupvalue: (func: (...args: any[]) => any | number, index: number, value: any) => void;
 }
